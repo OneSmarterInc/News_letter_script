6f7846155c1d88221e3f367645d41e8db2386ab3
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2024-04-24 18:49:38 +0530

    Update urls.py

--- osecurity/secure/urls.py
+++ osecurity/secure/urls.py
@@ -1,7 +1,10 @@
-from .views import Sign_in,AddCredentials,get_Credentials
+from .views import Sign_in,AddCredentials,get_Credentials,Sign_Out
 from django.urls import path
+
+
 urlpatterns = [
     path('add_credentials', AddCredentials.as_view()),
     path('get_credentials', get_Credentials.as_view()),
-    path('sign_in', Sign_in.as_view())
+    path('Sign_In', Sign_in.as_view()),
+    path('Sign_Out',Sign_Out.as_view())
 ]
